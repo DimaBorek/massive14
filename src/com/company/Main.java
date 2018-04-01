ab6cd6b01c575bf8a20702ce6379b31b2507cc7f
DimaBorek <drackon4ik@gmail.com>
2018-04-01 21:17:39 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = -20; i <21 ; i++) {
+            if (i%2 ==  0)
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i <100 ; i++) {
+            System.out.print(i + " ");
+
+        }
+    }
+
+}
